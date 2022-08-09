11e9a474014b0035d290c02d3bdcddb92247e02e
tylerbezona <tylerbezona@gmail.com>
2022-08-09 12:49:32 -0700

    First Commit

---
+++ archive-event.php
@@ -0,0 +1,22 @@
+<?php 
+get_header();
+pageBanner(array(
+    'title' => 'All Events',
+    'subtitle' => 'Stay updated!'
+))
+?>
+
+<div class="container container--narrow page-section">
+    <?php 
+    while(have_posts()) {
+        the_post(); 
+        get_template_part('template-parts/content-event');
+    }
+    echo paginate_links()
+    ?>
+    <hr class="section-break">
+    <p>Check out our past events archive <a href="<?php echo site_url('/past-events') ?>">here</a>!</p>
+</div>
+
+<?php get_footer();
+?>
---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '79011e3af0eb62b5a1a7');
